a48fc2d3cad78eda50b4253ca2af82c33dd2a259
Diego Bello <dbello@unal.edu.co>
2025-03-07 08:03:41 -0500

    Remove the number 1 from the player's board

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 class Player:
     def __init__(self, name="Jugador", current_loss=0):
         self.name = name
-        self.board = list(range(1,7)) + list(range(8,13))
+        self.board = list(range(2,7)) + list(range(8,13))
         self.current_loss = current_loss
 
