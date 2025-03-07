d06db38045591c31eb808047030ba6e671a1f34a
Diego Bello <dbello@unal.edu.co>
2025-03-07 09:35:54 -0500

    Add Match recap & make use of the Player.current_loss variable

--- match.py
+++ match.py
@@ -2,14 +2,17 @@ from dice import Dice
 from player import Player
 
 class Match:
-    def __init__(self, players, initial_bet, cace=100, dice=Dice()):
+    def __init__(self, players, initial_bet, cace=100, dice=Dice(), auto_mode=False):
+        self.auto_mode = auto_mode
         self.players = players
         self.dice = dice
         self.current_player = 0 # Index of the current player
         self.in_play = True
         self.price = initial_bet * len(players)
         self.cace = cace
-
+        
+        for player in self.players:
+            player.current_loss = -initial_bet
 
     def play(self):
         while self.in_play:
@@ -19,8 +22,9 @@ class Match:
             print(f"\n{player.name}, es tu turno.\n")
             print(f"Números faltantes para ganar\n\t{player.board}\n")
 
-            # Prompt the user to press Enter to roll the dice
-            input("Presiona Enter para lanzar los dados...\n")
+            if not self.auto_mode:
+                # Prompt the user to press Enter to roll the dice
+                input("Presiona Enter para lanzar los dados...\n")
 
             # Roll the dice
             roll = self.dice.roll()
@@ -33,8 +37,9 @@ class Match:
                 print(f"{player.board}")
                 print(f"¡Bien {player.name}! Te quedan {len(player.board)} número(s) para ganar.\n")
             elif roll == 7:
-                print(f"💰 ¡Maldito 7! {player.name} agrega {self.cace} a la bolsa de premio. 💰\n")
+                print(f"💰 ¡Maldito 7! {player.name} agrega {self.cace} a la bolsa de premio. 💰 ({self.price})\n")
                 self.price += self.cace
+                player.current_loss -= self.cace
                 self.current_player = (self.current_player + 1) % len(self.players)
                 continue
             else:
@@ -47,6 +52,7 @@ class Match:
             self.current_player = (self.current_player + 1) % len(self.players)
             count += 1
 
+        # If no player has that number, next player's turn
         if count == len(self.players):
             self.current_player = (self.current_player + 1) % len(self.players)
             return
@@ -55,14 +61,31 @@ class Match:
         if self.check_winner(self.players[self.current_player]):
             return
         
-        # If no player has that number, the turn is passed to the next player
-
-
         return
 
     def check_winner(self, player):
             if not player.board:
                 print(f"\n¡🏆 {player.name} ha ganado 💰 {self.price}!\n")
+                player.current_loss += self.price
                 self.in_play = False
+                self.match_recap()
                 return True
             return False
+    
+    def match_recap(self):
+        print(f"\n📝 Resumen de la partida 📝\n")
+
+        # Sort players by current_loss in descending order
+        sorted_players = sorted(self.players, key=lambda player: player.current_loss, reverse=True)
+
+        # Players' current loss
+        print(f"\t🪙 Pérdidas de los jugadores:")
+        for player in sorted_players:
+            print(f"\t\t{player.name}: {player.current_loss}")
+
+        # Numbers of the board remaining for each player
+        print(f"\n\t📋 Tablero de los jugadores:")
+        for player in sorted_players:
+            print(f"\t\t{player.name}: {player.board}")
+
+        return
